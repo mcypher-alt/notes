f71badc87506dc3f3035b1f790a1ab803990a69d
mcypher-alt <nikich-varganov@mail.ru>
2026-04-10 10:13:49 +0500

    Второй коммит

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 "use strict"
-const btn = document.querySelector('#btn-add');
+const openBtn = document.querySelector('#btn-add');
+const doneBtn = document.querySelector('#done-btn');
 const container = document.querySelector('.container');
+const formContainer = document.querySelector('#form-container');
+const noteName = document.querySelector('#note-name');
+const noteDesc = document.querySelector('#note-description');
+const resultDiv = document.querySelector('#result');
 
-btn.addEventListener('click', () => {
-    const noteDiv = document.createElement('div');
-    noteDiv.classList.add('box');
-    noteDiv.innerHTML = '<span>Новый <em>контент</em></span>';
-    container.appendChild(noteDiv);
+openBtn.addEventListener('click', () => {
+    formContainer.classList.remove('hidden');
+    openBtn.classList.add('hidden');
+    resultDiv.textContent = '';
 })
+
+doneBtn.addEventListener('click', () => {
+    formContainer.classList.add('hidden');
+    openBtn.classList.remove('hidden');
+    let noteValue = noteName.value.trim();
+    let noteDescription = noteDesc.value.trim();
+    let result = addNote(noteValue, noteDescription);
+    resultDiv.textContent = result;
+    noteName.value = '';
+    noteDesc.value = '';
+})
+
+function getNotes() {
+    const notes = localStorage.getItem('notes');
+    return notes ? JSON.parse(notes) : [];
+}
+
+function addNote(nameValue, descriptionValue) {
+    const notes = getNotes();
+    if (nameValue !== '' && descriptionValue !== '') {
+        const newNote = {
+        id: Date.now(),      // уникальный ID (хеш по времени)
+        title: nameValue,
+        text: descriptionValue,
+        createdAt: new Date().toISOString()
+    };
+    notes.push(newNote);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    return "Новая заметка добавлена!";
+    }
+    return "Поля ввода не могут быть пустыми!";
+}
